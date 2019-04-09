78d5d19a0e7e7188ca437856f8fd5e0fa4224d71
Rodrigo Nunes <rodrigo.tche.rn@gmail.com>
2019-04-09 01:48:09 -0300

    Migrate de clientes e testes de crud na tabela

--- picpay/tests/Feature/CrudClientTest.php
+++ picpay/tests/Feature/CrudClientTest.php
@@ -5,9 +5,13 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CrudClientTest extends TestCase
 {
+    
+    use DatabaseTransactions;
+    
     /**
      * A basic feature test example.
      *
@@ -18,5 +22,61 @@ class CrudClientTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }    
+
+    /**
+     * Teste de criação de registro na base de dados.
+     *
+     * @return void
+     */
+    public function testCreateTest()
+    {
+        $client_data = [
+            'client_id' => '065d8403-8a8f-484d-b602-9138ff7dedcf',
+            'name' => 'Wadson marcia',  
+            'username' => 'wadson.marcia'
+        ];
+        \App\Client::create($client_data);        
+        $this->assertDatabaseHas('clients', $client_data);
+    } 
+    
+    /**
+     * Teste de update na base de dados.
+     *
+     * @return void
+     */
+    public function testUpdateTest()
+    {
+        $client_data = [
+            'client_id' => '065d8403-8a8f-484d-b602-9138ff7dedcf',
+            'name' => 'Wadson marcia',  
+            'username' => 'wadson.marcia'
+        ];
+        $client = \App\Client::create($client_data);        
+        $this->assertDatabaseHas('clients', $client_data);
+        $new_data = ['name' => 'Wadson D. Marcia '];
+        $client->update($new_data);
+        $this->assertDatabaseHas('clients', $new_data);
+        $this->assertDatabaseMissing('clients', $client_data);
+    } 
+
+    /**
+     * Teste de deleção de registro na base de dados.
+     *
+     * @return void
+     */
+    public function testDeleteTest()
+    {
+        $client_data = [
+           'client_id' => '065d8403-8a8f-484d-b602-9138ff7dedcf',
+            'name' => 'Wadson marcia',  
+            'username' => 'wadson.marcia'
+        ];
+        $client = \App\Client::create($client_data);        
+        $this->assertDatabaseHas('clients', $client_data);    
+        
+        $client->delete();
+        $this->assertDatabaseMissing('clients', $client_data);
     }
+
 }
